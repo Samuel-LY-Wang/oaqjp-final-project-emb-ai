fec8d228e08b5873987e5ce600b9bcd77e017dcb
Samuel-LY-Wang <samuellywang@gmail.com>
2025-07-14 19:05:08 -0400

    made some code style changes to make pylint happy

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+'''
+The server that actually runs this program
+'''
+
 from flask import Flask, render_template, request
 from EmotionDetection import emotion_detection
 
@@ -5,19 +9,24 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
+    '''
+    Just renders everything needed to take in input, from index.html
+    '''
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def display():
+    '''
+    Takes in the AI's analysis of the text and prints out what was found
+    '''
     text_to_analyze = request.args.get("textToAnalyze")
     out = emotion_detection.emotion_detector(text_to_analyze)
-    out_str = f'''For the given statement, the system response is 
-    'anger': {out['anger']}, 
-    'disgust': {out['disgust']}, 
-    'fear': {out['fear']}, 
-    'joy': {out['joy']}, 
-    'sadness': {out['sadness']}. 
-    The dominant emotion is <b>{out['dominant_emotion']}</b>.'''
+    out_str = f'''For the given statement, the system response is 'anger': {out['anger']},
+     'disgust': {out['disgust']},
+      'fear': {out['fear']},
+       'joy': {out['joy']},
+        'sadness': {out['sadness']}.
+         The dominant emotion is <b>{out['dominant_emotion']}</b>.'''
     if out['dominant_emotion']:
         return out_str
     return "Invalid text! Please try again."
